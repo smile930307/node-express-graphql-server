03fc28df60b26abea114992b7eb176fa84e5f103
smile930307 <koriumemura0307@gmail.com>
2022-08-24 14:33:04 -0500

    update

--- server.js
+++ server.js
@@ -21,16 +21,7 @@ app.use('/graphql', expressGraphQL({
     schema: schema,
     rootValue: root,
     graphiql: true
-})
-
-app.use('/graphql', expressGraphQL({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-})
-
-
+}) 
 );
-
 
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
